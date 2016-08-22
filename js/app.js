d15b9b25f6bdef281fc288cc0ae05ec0ee847dd9
Jesus Medero <jmedero09@gmail.com>
2016-08-21 23:19:09 -0400

    oop way changing prefrence to true

--- js/app.js
+++ js/app.js
@@ -2,58 +2,253 @@
 
 $(document).ready(function(){
 
-var ask = ['Do ye like yer drinks strong?','Do ye like it with a salty tang?','Are ye a lubber who likes it bitter?','Would ye like a bit of sweetness with yer poison?','Are ye one for a fruity finish?'];
+// var ask = ['Do ye like yer drinks strong?','Do ye like it with a salty tang?','Are ye a lubber who likes it bitter?','Would ye like a bit of sweetness with yer poison?','Are ye one for a fruity finish?'];
+// var myDrink = [];
+// var currentQuestion = 0;
 
-var currentQuestion = 0;
+// function prefrence(question,answer){
 
-function prefrence(question,answer){
+// 	this.prefrenceQuestion = question;
 
-	this.prefrenceQuestion = question;
+// 	this.answer = answer;
+// }
+// function pantry(ingredients){
 
-	this.answer = answer;
+// 	this.ingredients = ingredients;
+// }
+
+// function drink(mixture){
+
+// 	this.mix = mixture;
+
+// }
+//  var strong = new pantry(['Glug of rum ','slug of whisky ','splash of gin ']);
+
+//  var salty = new pantry(['Olive on a stick ','salt-dusted rim ','rasher of bacon ']);
+
+//  var bitter = new pantry(['Shake of bitters ','splash of tonic ','twist of lemon peel ']);
+
+//  var sweet = new pantry(['Sugar cube ','spoonful of honey ','splash of cola ']);
+
+//  var Fruity = new pantry(['Slice of orange ','dash of cassis ','cherry on top ']);
+
+//  function showQuestion(){
+
+// 	$('p').append(ask[currentQuestion]);
+
+// }
+//  showQuestion();
+
+//  function mixDrink(drinkPref){
+
+//  	if(drinkPref.prefrenceQuestion ==="Do ye like yer drinks strong?"&& drinkPref.answer==='Yes'){
+
+//  		myDrink.push(strong.ingredients[Math.floor((Math.random() * 3) + 0)]);
+
+//  	} else if(drinkPref.prefrenceQuestion ==="Do ye like it with a salty tang?"&& drinkPref.answer==='Yes') {
+
+//   		myDrink.push(salty.ingredients[Math.floor((Math.random() * 3) + 0)]);
+		
+//  	} else if(drinkPref.prefrenceQuestion ==="Are ye a lubber who likes it bitter?"&& drinkPref.answer==='Yes') {
+
+//   		myDrink.push(bitter.ingredients[Math.floor((Math.random() * 3) + 0)]);
+		
+//  	} else if(drinkPref.prefrenceQuestion ==="Would ye like a bit of sweetness with yer poison?"&& drinkPref.answer==='Yes') {
+
+//   		myDrink.push(sweet.ingredients[Math.floor((Math.random() * 3) + 0)]);
+		
+//  	} else if(drinkPref.prefrenceQuestion ==="Are ye one for a fruity finish?"&& drinkPref.answer==='Yes') {
+
+//   		myDrink.push(Fruity.ingredients[Math.floor((Math.random() * 3) + 0)]);
+		
+//  	} 
+
+//  	var mixture = new drink(myDrink);
+
+//  	if(currentQuestion >= ask.length){
+//  		$('p').empty();
+
+//  		$('a').empty();
+
+//  		$('span').empty();
+
+//  		$('p').append("<span id = 'blue'>Ye Drink Be a:</span> " + mixture.mix);
+
+//  		$('#blurb').css("background-color", "#6b1d1d");
+
+//  	}
+
+//  }
+
+// 	$('a').click(function(event){
+
+// 		event.preventDefault();
+
+// 		var text = $(this).text();
+
+// 		$('#bartender-question').empty();
+
+// 		var drinkPrefrence = new prefrence(ask[currentQuestion],text);
+
+// 		currentQuestion++;
+
+// 		showQuestion();
+
+// 		mixDrink(drinkPrefrence);
+
+
+// 	});
+
+//Pantry------------------------------------------------------------------------------------
+
+var Pantry = function() {
+
+ 	this.strong = ['Glug of rum ','slug of whisky ','splash of gin '];
+
+	this.salty = ['Olive on a stick ','salt-dusted rim ','rasher of bacon '];
+
+ 	this.bitter = ['Shake of bitters ','splash of tonic ','twist of lemon peel '];
+
+ 	this.sweet = ['Sugar cube ','spoonful of honey ','splash of cola '];
+
+ 	this.fruity = ['Slice of orange ','dash of cassis ','cherry on top '];
+};
+//Ingredients------------------------------------------------------------------------------------
+
+var Ingredients = function(){
+
+	Pantry.call(this);
+
+	this.preferences = {
+
+		strong : false,
+
+		salty : false,
+
+		bitter : false,
+
+		sweet : false,
+
+	 	fruity : false
+	}
+
 }
-function pantry(ingredients){
 
-	this.ingredients = ingredients;
+Ingredients.prototype = Object.create(Pantry.prototype);//
+
+Ingredients.prototype.constructor = Ingredients;
+
+//Drink------------------------------------------------------------------------------------
+var Drink = function(){
+
+	Ingredients.call(this);
+
+	this.drink = [];
 }
 
- var strong = new pantry(['Glug of rum','slug of whisky','splash of gin']);
+Drink.prototype = Object.create(Ingredients.prototype);
 
- var salty = new pantry(['Olive on a stick','salt-dusted rim','rasher of bacon']);
+Drink.prototype.constructor = Drink;
 
- var bitter = new pantry(['Shake of bitters','splash of tonic','twist of lemon peel']);
+Drink.prototype.creatDrink = function(){
 
- var sweet = new pantry(['Sugar cube','spoonful of honey','splash of cola']);
-
- var Fruity = new pantry(['Slice of orange','dash of cassis','cherry on top']);
-
- function showQuestion(){
-
-	$('p').append(ask[currentQuestion]);
-	console.log(strong);
 
 }
- showQuestion();
 
-$('a').click(function(event){
+//Bartender------------------------------------------------------------------------------------
 
+ var Bartender = function(){
+
+ 	Drink.call(this);
+
+ 	this.questions = ['Do ye like yer drinks strong?','Do ye like it with a salty tang?','Are ye a lubber who likes it bitter?','Would ye like a bit of sweetness with yer poison?','Are ye one for a fruity finish?'];
+	
+	this.types = ['strong','salty','bitter','sweet','fruity'];
+
+	this.currentQuestion = 0;
+
+ }
+
+Bartender.prototype = Object.create(Drink.prototype);
+
+Bartender.prototype.constructor = Bartender;
+
+Bartender.prototype.askQuestion = function(){
+
+ 	$('p').append(this.questions[this.currentQuestion]);
+}
+
+ Bartender.prototype.init = function(){
+
+	this.askQuestion();
+}
+
+Bartender.prototype.clickHandler = function(){
+
+	var self = this;
+
+	$('a').click(function(event){
+	
 	event.preventDefault();
+
+	$('p').empty();
 
 	var text = $(this).text();
 
-	$('#bartender-question').empty();
+	if(text === 'Yes'){
 
-//created here so that it can have acess to the users yes or no selection
-	var drinkPrefrence = new prefrence(ask[currentQuestion],text);
+		self.preferences[self.types[self.currentQuestion]] = true;
 
-	currentQuestion++;
+	} else {
 
-	showQuestion();
+		self.preferences[self.types[self.currentQuestion]] = false;
+
+	}
+
+	// console.log(self.types[self.currentQuestion]);
+
+	// console.log(self.preferences[self.types[self.currentQuestion]]);
+	
+	self.currentQuestion++;
+
+	self.askQuestion(self.types[self.currentQuestion],text);
+
+	//console.log(self.preferences)
+
+	// console.log(self);
+
+
+
+});
+
+}
+
+var bartender = new Bartender();
+
+bartender.init();
+
+bartender.clickHandler();
+
+
+
+
+
 
 	
 
-});
-//what i want to do next is to make some if else statments and based off the index of the question i will supply the new object with a random option from the proper pantry
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
