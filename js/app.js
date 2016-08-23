6166b0c5c7131e18e9b3b590b0c379e67d1c4c6a
Jesus Medero <jmedero09@gmail.com>
2016-08-23 00:18:58 -0400

    fixed some spacing

--- js/app.js
+++ js/app.js
@@ -99,175 +99,152 @@ $(document).ready(function(){
 
 // 	});
 
-//Pantry------------------------------------------------------------------------------------
+	//Pantry------------------------------------------------------------------------------------
 
-var Pantry = function() {
+	var Pantry = function() {
 
- 	this.strong = ['Glug of rum ','slug of whisky ','splash of gin '];
+	 	this.strong = ['Glug of rum ','slug of whisky ','splash of gin '];
 
-	this.salty = ['Olive on a stick ','salt-dusted rim ','rasher of bacon '];
+		this.salty = ['Olive on a stick ','salt-dusted rim ','rasher of bacon '];
 
- 	this.bitter = ['Shake of bitters ','splash of tonic ','twist of lemon peel '];
+	 	this.bitter = ['Shake of bitters ','splash of tonic ','twist of lemon peel '];
 
- 	this.sweet = ['Sugar cube ','spoonful of honey ','splash of cola '];
+	 	this.sweet = ['Sugar cube ','spoonful of honey ','splash of cola '];
 
- 	this.fruity = ['Slice of orange ','dash of cassis ','cherry on top '];
-};
-//Ingredients------------------------------------------------------------------------------------
+	 	this.fruity = ['Slice of orange ','dash of cassis ','cherry on top '];
+	};
+	//Ingredients------------------------------------------------------------------------------------
 
-var Ingredients = function(){
+	var Ingredients = function(){
 
-	Pantry.call(this);
+		Pantry.call(this);
 
-	this.preferences = {
+		this.preferences = {
 
-		strong : false,
+			strong : false,
 
-		salty : false,
+			salty : false,
 
-		bitter : false,
+			bitter : false,
 
-		sweet : false,
+			sweet : false,
 
-	 	fruity : false
-	}
-
-}
-
-Ingredients.prototype = Object.create(Pantry.prototype);//
-
-Ingredients.prototype.constructor = Ingredients;
-
-//Drink------------------------------------------------------------------------------------
-var Drink = function(){
-
-	Ingredients.call(this);
-
-	this.drink = [];
-}
-
-Drink.prototype = Object.create(Ingredients.prototype);
-
-Drink.prototype.constructor = Drink;
-
-Drink.prototype.creatDrink = function(){
-
-	for(var prop in this.preferences) {
-
-		if(this.currentQuestion > this.questions.length-1 ) {
-			
-			if(this.preferences[prop] === true){
-
-				this.drink.push(this[prop][Math.floor((Math.random() * 3) + 0)]);
-
-			}
+		 	fruity : false
 		}
 
 	}
-return this.drink;
-}
 
-//Bartender------------------------------------------------------------------------------------
+	Ingredients.prototype = Object.create(Pantry.prototype);//
 
- var Bartender = function(){
+	Ingredients.prototype.constructor = Ingredients;
 
- 	Drink.call(this);
+	//Drink------------------------------------------------------------------------------------
+	var Drink = function(){
 
- 	this.questions = ['Do ye like yer drinks strong?','Do ye like it with a salty tang?','Are ye a lubber who likes it bitter?','Would ye like a bit of sweetness with yer poison?','Are ye one for a fruity finish?'];
-	
-	this.types = ['strong','salty','bitter','sweet','fruity'];
+		Ingredients.call(this);
 
-	this.currentQuestion = 0;
-
- }
-
-Bartender.prototype = Object.create(Drink.prototype);
-
-Bartender.prototype.constructor = Bartender;
-
-Bartender.prototype.askQuestion = function(){
-
- 	$('p').append(this.questions[this.currentQuestion]);
-}
-
- Bartender.prototype.init = function(){
-
-	this.askQuestion();
-}
- Bartender.prototype.display = function(){
-
-	if(this.currentQuestion > this.questions.length-1 ){
-
-		$('p').empty();
-
-		$('a').empty();
-
-		$('span').empty();
-
-		$('p').append("<span id = 'blue'>Ye Drink Be a:</span> " + this.creatDrink());
-
-		$('#blurb').css("background-color", "#6b1d1d");		
+		this.drink = [];
 	}
 
-}
-Bartender.prototype.clickHandler = function(){
+	Drink.prototype = Object.create(Ingredients.prototype);
 
-	var self = this;
+	Drink.prototype.constructor = Drink;
 
-	$('a').click(function(event){
-	
-	event.preventDefault();
+	Drink.prototype.creatDrink = function(){
 
-	$('p').empty();
+		for(var prop in this.preferences) {
 
-	var text = $(this).text();
+			if(this.currentQuestion > this.questions.length-1 ) {
+				
+				if(this.preferences[prop] === true){
 
-	if(text === 'Yes'){
+					this.drink.push(this[prop][Math.floor((Math.random() * 3) + 0)]);
 
-		self.preferences[self.types[self.currentQuestion]] = true;
+				}
+			}
 
-	} else {
+		}
+	return this.drink;
+	}
 
-		self.preferences[self.types[self.currentQuestion]] = false;
+	//Bartender------------------------------------------------------------------------------------
+
+	 var Bartender = function(){
+
+	 	Drink.call(this);
+
+	 	this.questions = ['Do ye like yer drinks strong?','Do ye like it with a salty tang?','Are ye a lubber who likes it bitter?','Would ye like a bit of sweetness with yer poison?','Are ye one for a fruity finish?'];
+		
+		this.types = ['strong','salty','bitter','sweet','fruity'];
+
+		this.currentQuestion = 0;
+
+	 }
+
+	Bartender.prototype = Object.create(Drink.prototype);
+
+	Bartender.prototype.constructor = Bartender;
+
+	Bartender.prototype.askQuestion = function(){
+
+	 	$('p').append(this.questions[this.currentQuestion]);
+	}
+
+	 Bartender.prototype.init = function(){
+
+		this.askQuestion();
+	}
+	 Bartender.prototype.display = function(){
+
+		if(this.currentQuestion > this.questions.length-1 ){
+
+			$('p').empty();
+
+			$('a').empty();
+
+			$('span').empty();
+
+			$('p').append("<span id = 'blue'>Ye Drink Be a:</span> " + this.creatDrink());
+
+			$('#blurb').css("background-color", "#6b1d1d");		
+		}
 
 	}
-	
-	self.currentQuestion++;
+	Bartender.prototype.clickHandler = function(){
 
-	self.askQuestion(self.types[self.currentQuestion],text);
+		var self = this;
 
-	bartender.display();
+		$('a').click(function(event){
+		
+			event.preventDefault();
 
-});
+			$('p').empty();
 
-}
+			var text = $(this).text();
 
-var bartender = new Bartender();
+			if(text === 'Yes'){
 
-bartender.init();
+				self.preferences[self.types[self.currentQuestion]] = true;
 
-bartender.clickHandler();
+			} else {
 
+				self.preferences[self.types[self.currentQuestion]] = false;
 
+			}
+			
+			self.currentQuestion++;
 
+			self.askQuestion(self.types[self.currentQuestion],text);
 
+			bartender.display();
 
+		});
 
+	}
 
+		var bartender = new Bartender();
 
+		bartender.init();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		bartender.clickHandler();
 });
